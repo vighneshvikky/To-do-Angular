b4e907a8403e1d9b4bf1dc1095c1f8cca1b6c006
Vighnesh SL <vighnesh55642@gmail.com>
2025-02-01 16:32:16 +0530

    updated UI

--- src/app/to-do.service.ts
+++ src/app/to-do.service.ts
@@ -1,64 +1,83 @@
 import { Injectable } from '@angular/core';
 import { Items } from './to-do-item/to-do-item-model';
+import Swal from 'sweetalert2';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToDoService {
-  private todos:Items[] = [];
+  private todos: Items[] = [];
   private counter = 1;
 
-
-  constructor(){
+  constructor() {
     this.loadTodos();
   }
 
-  addTodo(title: string): void{
-   
-if(title.trim()){
-  this.todos.push({
-    id: this.counter++,
-    completed:false,
-    title
-  })
-}
+  addTodo(title: string): void {
+    if (title.trim()) {
+      const isDuplicate = this.todos.find(
+        (todo) => todo.title.toLowerCase() === title.toLowerCase()
+      );
 
-this.saveTodo()
+      if (isDuplicate) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Duplicate Todo',
+          text: 'This todo already exists!',
+          confirmButtonText: 'OK',
+        });
+      } else {
+        this.todos.push({
+          id: this.counter++,
+          completed: false,
+          title,
+        });
+      }
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Empty Todo',
+        text: 'Please enter a valid todo!',
+        confirmButtonText: 'OK',
+      });
+    }
+
+    this.saveTodo();
   }
 
-  getTodo(): Items[]{
+  getTodo(): Items[] {
     return this.todos;
   }
 
-  toggleCompletion(id: number){
-    const task = this.todos.find((t) => t.id === id)
-    if(task){
-      task.completed = !task.completed
+  toggleCompletion(id: number) {
+    const task = this.todos.find((t) => t.id === id);
+    if (task) {
+      task.completed = !task.completed;
     }
-    this.saveTodo()
+    this.saveTodo();
   }
 
+  removeTodo(taskId: number): void {
+    this.todos = this.todos.filter((t) => t.id !== taskId);
+    this.saveTodo();
+  }
 
-   removeTodo(taskId: number): void{
-   this.todos = this.todos.filter((t) => t.id   !== taskId)
-   this.saveTodo()
-   }
-
-   private saveTodo(): void {
+  private saveTodo(): void {
     localStorage.setItem('todos', JSON.stringify(this.todos));
-    localStorage.setItem('id',JSON.stringify(this.counter))
-   }
+    localStorage.setItem('id', JSON.stringify(this.counter));
+  }
 
-   private loadTodos(): void {
+  private loadTodos(): void {
     const savedTodo = localStorage.getItem('todos');
     const savedId = localStorage.getItem('id');
 
-    if(savedTodo){
-      this.todos = JSON.parse(savedTodo)
+    if (savedTodo) {
+      this.todos = JSON.parse(savedTodo);
     }
-    if(savedId){
-      this.counter = JSON.parse(savedId)
-    }
-   }
+    if (savedId) {
 
+      
+      this.counter = JSON.parse(savedId);
+    }
+  }
 }
